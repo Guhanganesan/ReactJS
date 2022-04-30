99b7a040ecdf28c3df0c0703881df1c7d1d6350c
guhanganesan <raghavaguhan@gmail.com>
2022-05-01 00:37:56 +0530

    how to add image in react js

--- myapp/src/components/ContactCard.js
+++ myapp/src/components/ContactCard.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import user from '../images/user.png';
 
 const ContactCard = (props)=>{
     const {id, name, email} = props.contacts;
     return (
         <div className="item">
             <div className="content">
+                <img className="ui avatar image" src={user}></img>
                 <div className="header">
                     Id: {id}
                 </div>
